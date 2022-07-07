420257cb65083959d78890f51b7536a576cc915f
Sakshi Jagtap <99532147+sakshikjagtap@users.noreply.github.com>
2022-07-07 12:52:54 +0530

    added signup functionality

--- src/app/handlers/loginHandler.js
+++ src/app/handlers/loginHandler.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
 
 const sessions = {};
+const users = {};
 
-const createSession = (req, res) => {
+
+const isvalidUser = (username, password, users) => {
+  const user = users[username];
+
+  if (!user) {
+    return false;
+  }
+
+  if (user.password === password) {
+    return true;
+  }
+  return false;
+}
+
+const createSession = (req, res, sessions) => {
   const date = new Date();
   const sessionId = date.getTime();
   res.setHeader('set-cookie', `sessionId=${sessionId}`);
@@ -22,17 +37,23 @@ const loginHandler = (req, res, next) => {
     }
   }
 
-  const { username } = req.bodyParams;
-  if (req.method === 'POST' && username) {
-    createSession(req, res);
-    res.statusCode = 302;
-    res.setHeader('location', '/guest-book');
-    res.end();
-    return;
+  const { username, password } = req.bodyParams;
+  if (req.method === 'POST' && pathname === '/login') {
+    if (isvalidUser(username, password, users)) {
+      createSession(req, res, sessions);
+      res.statusCode = 302;
+      res.setHeader('location', '/guest-book');
+      res.end();
+      return;
+    }
+    if (!isvalidUser(username, password, users)) {
+      res.end(loginPage);
+      return;
+    }
   }
-
   next();
 };
+
 
 const logoutHandler = (req, res, next) => {
   const { pathname } = req.url;
@@ -51,4 +72,29 @@ const logoutHandler = (req, res, next) => {
   return;
 };
 
-module.exports = { loginHandler, logoutHandler };
+const signupHandler = (req, res, next) => {
+  const { pathname } = req.url;
+  const signupPage = fs.readFileSync('public/signup.html', 'utf-8')
+  if (pathname !== '/signup') {
+    next();
+    return;
+  }
+
+  if (req.method === 'GET') {
+    res.end(signupPage);
+    return;
+  }
+
+  if (req.method === 'POST') {
+    const { username, password } = req.bodyParams;
+    if (username && password) {
+      users[username] = { username, password };
+      res.statusCode = 302;
+      res.setHeader('location', '/guest-book');
+      res.end();
+      return;
+    }
+  }
+};
+
+module.exports = { loginHandler, logoutHandler, signupHandler };
