f0760bc71e9b9fe5179f98c462fcb0bca6dff86c
Sakshi Jagtap <99532147+sakshikjagtap@users.noreply.github.com>
2022-07-07 11:14:55 +0530

    Added logout functionality

--- src/app/handlers/loginHandler.js
+++ src/app/handlers/loginHandler.js
@@ -34,4 +34,21 @@ const loginHandler = (req, res, next) => {
   next();
 };
 
-module.exports = { loginHandler };
+const logoutHandler = (req, res, next) => {
+  const { pathname } = req.url;
+  const { sessionId } = req.session;
+
+  if (pathname !== '/logout') {
+    next();
+    return;
+  }
+
+  delete sessions[sessionId];
+  res.setHeader('Set-Cookie', 'sessionId=0;Max-Age=0');
+  res.statusCode = 302;
+  res.setHeader('location', '/');
+  res.end();
+  return;
+};
+
+module.exports = { loginHandler, logoutHandler };
